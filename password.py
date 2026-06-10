f78901bb95084148d4a79d5ce466eb4cc14bb37c
lynnadie-png <lynn.adie@student.uts.edu.au>
2026-06-10 20:23:59 +1000

    Update password.py

--- password.py
+++ password.py
@@ -1,6 +1,7 @@
 import random 
 import string 
 
+print("Password Generator")
 
 length = int(input("Password length: "))
 
@@ -8,4 +9,5 @@ characters = string.ascii_letters + string.digits + "!@#$%^&*"
 
 password = ''.join(random.choice(characters) for _ in range(length))
 
+print("\nGenerated Password:")
 print(password)
